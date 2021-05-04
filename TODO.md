b0b8b02437b32688ccca795037dfb3654a93754b
Felipe Scarel <fbscarel@gmail.com>
2021-05-04 14:35:36 -0300

    initial import

---
+++ TODO.md
@@ -0,0 +1,6 @@
+# TODO list
+
+- More game options
+- Syslog-style log to file
+- Help command
+- User tutorial
---
+++ seedbot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# This example requires the 'members' privileged intents
+
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+@bot.command()
+async def mmseed(ctx):
+    await ctx.message.author.send('ou aqui')
+
+@bot.event
+async def on_message(message):
+    if isinstance(message.channel, discord.DMChannel):
+        channel = bot.get_channel(int(780745477842403338))
+        await channel.send(f"{message.author} sent:\n```{message.content}```\n{message.attachments[0].url}")
+    await bot.process_commands(message)
+
+bot.run('ODM5MDU0OTcyMjc2MTc4OTQ0.YJEE1g.-i1tsNmfczwvvc_Oz9T7eVchGZ4')
