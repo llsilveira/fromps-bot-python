9c35d8d34f10c072576570e4322de4ae817554f8
Felipe Scarel <fbscarel@gmail.com>
2021-05-05 06:19:40 -0300

    add help; add more games; split URL config from code

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # zrbr-seedbot
 Discord bot to manage rando seed for the Zelda Randomizer Brazil server
+
+Install deps with 'pip3 install -r requirements.txt' before running
--- seedbot.py
+++ seedbot.py
@@ -3,6 +3,7 @@
 
 import discord
 from discord.ext import commands
+from benedict import benedict
 
 intents = discord.Intents.default()
 intents.members = True
@@ -16,9 +17,25 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-@bot.command()
+@bot.command(name = 'alttpseed', help = 'Solicitar seed da semanal de A Link to the Past')
+async def alttpseed(ctx):
+    await ctx.message.author.send(g['ALTTP'])
+
+@bot.command(name = 'ootseed', help = 'Solicitar seed da semanal de Ocarina of Time')
+async def ootseed(ctx):
+    await ctx.message.author.send(g['OoT'])
+
+@bot.command(name = 'mmseed', help = 'Solicitar seed da semanal de Majora\'s Mask')
 async def mmseed(ctx):
-    await ctx.message.author.send('ou aqui')
+    await ctx.message.author.send(g['MM'])
+
+@bot.command(name = 'smz3seed', help = 'Solicitar seed da semanal de SMZ3')
+async def smz3seed(ctx):
+    await ctx.message.author.send(g['SMZ3'])
+
+@bot.command(name = 'pkmncrystalseed', help = 'Solicitar seed da semanal de Pokémon Crystal')
+async def pkmncrystalseed(ctx):
+    await ctx.message.author.send(g['PkmnCrystal'])
 
 @bot.event
 async def on_message(message):
@@ -26,5 +43,7 @@ async def on_message(message):
         channel = bot.get_channel(int(780745477842403338))
         await channel.send(f"{message.author} sent:\n```{message.content}```\n{message.attachments[0].url}")
     await bot.process_commands(message)
+
+g = benedict.from_yaml('games.yaml')
 
 bot.run('TOKEN')
